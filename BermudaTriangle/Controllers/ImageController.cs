3bb060822bcef155f64273a58ec66c7e582ec36e
Sean Kearney <sean.kearney@c2software.com>
2019-04-06 12:27:08 +0100

    WepAPI controller added

---
+++ BermudaTriangle/Controllers/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BermudaTriangle.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BermudaTriangle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        public List<Coordinate> Get(string gRef)
+        {
+            IImage t = new Triangle();
+            List<Coordinate> locations = t.WhereAmI(gRef);
+
+            return locations;
+        }
+    }
+}
