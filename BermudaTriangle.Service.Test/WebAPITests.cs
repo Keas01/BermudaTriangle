164f11acfc5f9a136572eb868989541094534435
Sean Kearney <sean.kearney@c2software.com>
2019-04-06 12:27:19 +0100

    WebAPI test s added

---
+++ BermudaTriangle.Service.Test/WebAPITests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BermudaTriangle.Service.Test
+{
+    [TestClass]
+    internal class WebAPITests
+    {
+        private string _url = "http://sean/api/image";
+        private HttpClient _client;
+
+        public WebAPITests()
+        {
+            _client = new HttpClient();
+        }
+
+        [TestMethod]
+        public void GetByGridRef_ValidGridRef_LocationsReturned()
+        {
+            var result = _client.GetAsync(_url).Result;
+        }
+    }
+}
